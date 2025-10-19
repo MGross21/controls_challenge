349b8b5edb8c2cfa26418b71a8d6b6bdcdb83cb8
MGross21 <mgstinger521@gmail.com>
2025-10-18 20:38:46 -0700

    try 1

--- controllers/pid.py
+++ controllers/pid.py
@@ -2,19 +2,32 @@ from . import BaseController
 import numpy as np
 
 class Controller(BaseController):
-  """
-  A simple PID controller
-  """
-  def __init__(self,):
-    self.p = 0.195
-    self.i = 0.100
-    self.d = -0.053
+  def __init__(self):
+    self.p = 0.3
+    self.i = 0.15
+    self.d = -0.1
     self.error_integral = 0
     self.prev_error = 0
+    self.max_integral = 2.0
+    self.prev_output = 0
+    self.alpha = 0.5
 
   def update(self, target_lataccel, current_lataccel, state, future_plan):
-    error = (target_lataccel - current_lataccel)
+    error = target_lataccel - current_lataccel
+    
+    # Update integral term with clamping
     self.error_integral += error
+    self.error_integral = np.clip(self.error_integral, -self.max_integral, self.max_integral)
+    
+    # Calculate derivative term
     error_diff = error - self.prev_error
     self.prev_error = error
-    return self.p * error + self.i * self.error_integral + self.d * error_diff
+    
+    # Compute PID output
+    output = self.p * error + self.i * self.error_integral + self.d * error_diff
+    
+    # Apply smoothing with previous output
+    output = self.alpha * output + (1 - self.alpha) * self.prev_output
+    self.prev_output = output
+    
+    return output
